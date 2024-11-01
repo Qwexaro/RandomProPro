706def7d085d8a219f7183e71e5273af4a971b6f
Sergey0909087 <Sergikstar25.gold@mail.ru>
2024-11-01 21:29:52 +0300

    main

---
+++ RandomProPro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandomProPro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
