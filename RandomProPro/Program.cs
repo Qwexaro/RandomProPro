c59a68bfdf7d05948367b0dd44e0d2dbf69e887d
Sergey0909087 <Sergikstar25.gold@mail.ru>
2024-11-01 21:55:38 +0300

    main

--- RandomProPro/Program.cs
+++ RandomProPro/Program.cs
@@ -9,7 +9,11 @@ namespace RandomProPro
             for (int i = 0; i < 50; i++) Console.Write("#");
             Console.WriteLine("\nВведи ка число: ");
             int num = Convert.ToInt32(Console.ReadLine());
+            
+            // реализация bad алгоритма представлена на 15 строке
+            
             int rand_num = (num + 7932) * 5 % 123;
+            
             Console.WriteLine("Рандомно преисполненное: " + rand_num);
         }
     }
